fea39c86d970a1a23442dd091909623a4de0fd70
Emmanuel Desprez <emmanuel.desprez@gmail.com>
2023-11-28 09:45:07 +0100

    refactor terre 01

--- terre01.swift
+++ terre01.swift
@@ -3,6 +3,13 @@ import Foundation
 
 //Créez un programme qui affiche son nom de fichier.
 
-let fileName = "\(#file)"
 
-print(fileName)
+let currentFileURL = URL(fileURLWithPath: CommandLine.arguments[0])
+let currentFileName = currentFileURL.lastPathComponent
+
+if (currentFileName == "") {
+    print("Nous n'avons pas reçu de niom de fichier")
+} else {
+    print("Le nom du fichier courant est : \(currentFileName)")
+}
+
