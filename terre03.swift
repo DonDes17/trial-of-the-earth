a23ed5b7e7f56f514d2d14ff4966e35c5dda17e8
Emmanuel Desprez <emmanuel.desprez@gmail.com>
2023-11-29 12:11:18 +0100

    terre 03 done

---
+++ terre03.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+//Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne.
+
+let alpha: String = "abcdefghijklmnopqrstuvwxyz"
+
+if CommandLine.arguments.count <= 1 {
+    print("Aucun argument passé.")
+} else {
+    let argument: String = CommandLine.arguments[1]
+    var distance: Int = 0
+    var restOfAlphabet: String = ""
+    
+    if let index = alpha.firstIndex(where: { $0.isLetter && $0.lowercased() == argument.lowercased() }) {
+        distance = alpha.distance(from: alpha.startIndex, to: index)
+    }
+    
+    for (index, letter) in alpha.enumerated() {
+        if index >= distance {
+            restOfAlphabet += String(letter)
+        }
+    }
+    
+    print(restOfAlphabet)
+}
+
